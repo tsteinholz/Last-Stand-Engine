140ae0cf8b4720f458e6aa798b432bd09200f488
Joshua Freedman <npjoshf@gmail.com>
2015-06-07 15:00:33 -0700

    Adding custom logger

---
+++ Source/Utils/Log.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Log {
+public:
+    Log(const std::string &fName) {
+        std::cout << fName << ": ";
+    }
+
+    template<class T>
+    Log &operator<<(const T &a) {
+        std::cout << a;
+        return *this;
+    }
+
+    ~Log() {
+        std::cout << " [eom]" << std::endl;
+    }
+};
+
+#define LOG Log(__FUNCTION__)
