eed4b331dd6b76be6be1a89b9c4d54b5217c0c4e
Thomas Steinholz <tsteinholz@yahoo.com>
2015-06-13 02:06:15 -0400

    Linux Build Dependencies Script

---
+++ LinuxDeps.sh
@@ -0,0 +1,19 @@
+echo Last Stand Studio Dependencies
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+VER=$(uname -r)
+
+if [ -f "/etc/arch-release" ]; then
+	echo Arch Linux Distro Detected
+	OS=Arch
+	pacman -S sdl2 jsoncpp
+elif [ -f etc/debian_version ]; then
+	echo Debian Based Distro Detected
+	OS=Debian
+elif [ -f /etc/redhat-release ]; then
+	echo RedHat Distro Detected
+	OS=RedHat
+else
+	echo Current Distro is not supported
+fi
