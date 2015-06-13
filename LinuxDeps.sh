61ee7243e05beae2b3858ef3d90ef85f3dc24f13
Thomas Steinholz <tsteinholz@yahoo.com>
2015-06-13 02:12:23 -0400

    Linux Build Dependencies Script

--- LinuxDeps.sh
+++ LinuxDeps.sh
@@ -34,16 +34,22 @@ ARCH=$(uname -m)
 VER=$(uname -r)
 
 if [ -f "/etc/arch-release" ]; then
-	echo  [*] Arch Linux Distro Detected
+
+	echo  [*] Arch Linux Distro Detected!
 	OS=Arch
 	pacman -S sdl2 jsoncpp
+
 elif [ -f etc/debian_version ]; then
-	echo  [*] Debian Based Distro Detected
+
+	echo  [*] Debian Based Distro Detected!
 	OS=Debian
 	apt-get install dev-libsdl2
+
 elif [ -f /etc/redhat-release ]; then
-	echo  [*] RedHat Distro Detected
+
+	echo  [*] RedHat Distro Detected!
 	OS=RedHat
+
 else
-	echo Current Distro is not supported
+	echo  [*] Current Distro is not supported!
 fi
